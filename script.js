9b4211c5e86d4f663c2dbfc841d460fb3d659220
unknown <n.hasratova@mail.ru>
2024-02-03 22:37:33 +0400

    .

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+// //1
+let fullName = prompt('Enter your full name')
+// console.log(`Hello ${fullName}`)
+// //2
+// let value1 = prompt('Enter first number')
+// let value2 = prompt('Enter second number')
+// console.log(+value1 + Number(value2))
+// console.log(+value1 - Number(value2))
+// console.log(+value1 * Number(value2))
+// console.log(+value1 / Number(value2))
+// console.log(+value1 % Number(value2))
+
+// //3
+// console.log(`${12345*98765}`);
+// console.log(`${12345*98765}`[5]);
+
+// //4
+// let value = prompt("ввести четырёхзначное число")
+// console.log(+`${value}`[0] + Number(`${value}`[1]) + Number(`${value}`[2]) + Number(`${value}`[3]))
+
+// //5
+
+let devide = fullName.split(' ')
+// let firstname = devide[0]
+// let surname = devide[1]
+// console.log(`Hello ${firstname}!`)
+
+
 //1
-console.log('My name is Nisa');
-//2
-const name = 'Nisa'
-console.log(name)
+// let price = prompt('Enter a numbe in 530.90 format')
+// let space = price.split('.')
+// let rubl = space[0]
+// let cent = space[1]
+
+// console.log(`${rubl} руб. ${cent} коп. `)
+
+// //2
+// let moneyAmount = prompt('Enter you salary')
+// moneyAmount = moneyAmount*87/100
+// console.log(moneyAmount)
+
 //3
-let bookPrice = 5;
-let bookAmount = 300;
-console.log(bookAmount * bookPrice);
-//4
+
+
+
+
