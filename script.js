eb145007b51abd2198f5fd38f5223fc0584d6144
unknown <n.hasratova@mail.ru>
2024-02-13 21:51:25 +0400

    13.02.2023

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//1st presentation
+//1st presentation homework
 //task 1
 console.log('My name is Nisa');
 //task2
@@ -32,52 +32,25 @@ console.log(fullName.length-1)
 
 
 
+//task1
+let kmhSpeed = 75
+let msSpeed = kmhSpeed*1000/3600
+console.log(msSpeed)
+//task2
+let num3 = 57
+console.log(num3**5)
+console.log(num3%3)
+//task3
+let firstName3 = 'Alan'
+let lastName3 = 'Turing'
+console.log("Sizin adınız " + firstName3 + ' ' + lastName3 +  ".Tanışlığımıza çox şadam!")
+//task4
+console.log(`Sizi bir daha görməyimizə şadıq,${firstName3} ${lastName3}`)
+//task5
+let fullName2 = 'Alan Turing'
+console.log(fullName2.length-1)
 
 
-
-
-// //1
-// let fullName = prompt('Enter your full name')
-// console.log(`Hello ${fullName}`)
-// //2
-// let value1 = prompt('Enter first number')
-// let value2 = prompt('Enter second number')
-// console.log(+value1 + Number(value2))
-// console.log(+value1 - Number(value2))
-// console.log(+value1 * Number(value2))
-// console.log(+value1 / Number(value2))
-// console.log(+value1 % Number(value2))
-
-// //3
-// console.log(`${12345*98765}`);
-// console.log(`${12345*98765}`[5]);
-
-// //4
-// let value = prompt("ввести четырёхзначное число")
-// console.log(+`${value}`[0] + Number(`${value}`[1]) + Number(`${value}`[2]) + Number(`${value}`[3]))
-
-// //5
-
-// let devide = fullName.split(' ')
-// let firstname = devide[0]
-// let surname = devide[1]
-// console.log(`Hello ${firstname}!`)
-
-
-//1
-// let price = prompt('Enter a numbe in 530.90 format')
-// let space = price.split('.')
-// let rubl = space[0]
-// let cent = space[1]
-
-// console.log(`${rubl} руб. ${cent} коп. `)
-
-// //2
-// let moneyAmount = prompt('Enter you salary')
-// moneyAmount = moneyAmount*87/100
-// console.log(moneyAmount)
-
-//3
 
 
 
