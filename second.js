8947db730b560f984a5477a029a4a83114bc5caa
Nisa Hasratova <n.hasratova@mail.ru>
2024-02-15 12:02:26 +0400

    function

--- second.js
+++ second.js
@@ -1,55 +1,10 @@
-// //presentation 3
-// //1
-// let fullName = prompt('Enter your full name')
-// console.log(`Hello ${fullName}`)
-// //2
-// let value1 = prompt('Enter first number')
-// let value2 = prompt('Enter second number')
-// console.log(+value1 + Number(value2))
-// console.log(+value1 - Number(value2))
-// console.log(+value1 * Number(value2))
-// console.log(+value1 / Number(value2))
-// console.log(+value1 % Number(value2))
-
-// //3
-// console.log(`${12345*98765}`);
-// console.log(`${12345*98765}`[5]);
-
-// //4
-// let value = prompt("ввести четырёхзначное число")
-// console.log(+`${value}`[0] + Number(`${value}`[1]) + Number(`${value}`[2]) + Number(`${value}`[3]))
-
-// //5
-
-// let devide = fullName.split(' ')
-// let firstname = devide[0]
-// let surname = devide[1]
-// console.log(`Hello ${firstname}!`)
-
-
-
-
 // //presentation4
-// //1
-// let price = prompt('Enter a numbe in 530.90 format')
-// let space = price.split('.')
-// let rubl = space[0]
-// let cent = space[1]
 
-// console.log(`${rubl} руб. ${cent} коп. `)
 
 // //2
 // let moneyAmount = prompt('Enter you salary')
 // moneyAmount = moneyAmount*87/100
 // console.log(moneyAmount)
-
-// //3
-// let fullName1= prompt('Напишите Ф.И.О.')
-// let yourName = fullName.split(' ')
-// let name =yourName[1]
-// let surname1 = yourName[0]
-// let patronymic = yourName [2]
-// console.log(surname +' '+ name[0]+ '. ' +  patronymic[0]+ '.')
 
 
 
@@ -63,6 +18,7 @@
 // else{
 //     console.log('Access granted');
 // }
+
 // //task3
 // let enter = prompt('Enter 3 numbers')
 // split = enter.split(' ')
@@ -84,6 +40,7 @@
 // }else if((firstNum == secondNum) && (secondNum==thirdNum) &&(firstNum== thirdNum)){
 //     console.log('Два или три введённых числа равны');
 // }else ('Введено некорректное значение');
+
 //task4
 // let year = prompt('Enter a random year')
 // if(year%4 == 0){
@@ -98,7 +55,7 @@
 
 
 
-//presentation6
+//presentation6 swich case
 // //task1
 // let id = prompt('введите id товара:');
 // switch(id){
@@ -117,15 +74,80 @@
 //     default:
 //         alert('Нет в наличии');    
 // }
+
 // //task2
 // let gender = prompt('Enter your gender as F or M')
 // let whatIsYourGender = gender == 'F' || gender == 'f'?
 // 'Female': 'Male'
 // console.log(whatIsYourGender)
-//task3
-let date = prompt('Enter the date in this format "YYYY.MM.DD"')
-dote = date.split('.')
-let year = dote[0]
-let month = dote[1]
-let day = dote[2]
 
+// task3
+// let date = prompt('Enter the date in this format "YYYY.MM.DD"')
+// dote = date.split('.')
+// let year = dote[0]
+// let month = dote[1]
+// let day = dote[2]
+// switch(month){
+//     case '01':
+//         dote[1] = 'January';
+//     break;
+//     case '02':
+//         dote[1] = 'February';
+//     break;
+//     case '03':
+//         dote[1] = 'March';
+//     break;
+//     case '04':
+//         dote[1] = 'April';
+//     break;
+//     case '05':
+//         dote[1] = 'May';
+//     break;
+//     case '06':
+//         dote[1] = 'June';
+//     break;
+//     case '07':
+//         dote[1] = 'July';
+//     break;
+//     case '08':
+//         dote[1] = 'August';
+//     break;
+//     case '09':
+//         dote[1] = 'September';
+//     break;
+//     case '10':
+//         dote[1] = 'October';
+//     break;
+//     case '11':
+//         dote[1] = 'November';
+//     break;
+//     case '12':
+//         dote[1] = 'December';
+//     break;
+// }
+// console.log(`${day} ${dote[1]} ${year} года`);
+
+// //presentation7 functions
+// //task1
+// let Fullname = prompt('Enter your name')
+// function logGreeting (){
+//     console.log(`${Fullname} is a developer`)
+// }
+// logGreeting()
+// //task2
+// function yourName1(Fullname1){
+//     console.log(`${Fullname1} is active now`);
+// }
+// yourName1('Nisa Hasratova')
+// yourName1('Said Hasratli')
+//task3
+function maxNum (num1, num2){
+    if(num1<num2){
+        console.log(num2)
+    }
+    else{
+        console.log(num1);
+    }
+}
+// maxNum(2, 3) //нужно переделать для 3х элементов
+// //task4
